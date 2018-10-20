2192de68260b1014420640aa97d4cac58c0cc23d
Harrison Wall <harrisonwall99@yahoo.com>
2018-10-20 13:09:49 -0400

    Added team B

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,12 +2,92 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score)
+    {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Increments Team A's score to 3
+     */
+    public void plus3TeamA(View view)
+    {
+       scoreTeamA += 3;
+       displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increments Team A's score to 2
+     */
+    public void plus2TeamA(View view)
+    {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increments Team A's score to 1
+     */
+    public void plus1TeamA(View view)
+    {
+        scoreTeamA ++;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score)
+    {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Increments Team B's score to 3
+     */
+    public void plus3TeamB(View view)
+    {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increments Team B's score to 2
+     */
+    public void plus2TeamB(View view)
+    {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increments Team B's score to 1
+     */
+    public void plus1TeamB(View view)
+    {
+        scoreTeamB ++;
+        displayForTeamB(scoreTeamB);
     }
 }
