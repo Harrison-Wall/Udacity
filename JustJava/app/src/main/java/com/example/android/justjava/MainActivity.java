5474a6dda594a90c05ea7d715c9cce9294005d46
Harrison Wall <harrisonwall99@yahoo.com>
2018-10-21 11:09:04 -0400

    Updated Layout text, added orderSummary method.

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -23,65 +23,86 @@ public class MainActivity extends AppCompatActivity
      * Global variables for the number of Coffees and their price
      */
     int quantity = 2;
-    int price = 5;
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view)
     {
-        String priceMessage = "Total: $" + (quantity * price);
-        priceMessage += "\nThank You!";
+        int price = calculatePrice();
 
-        if( quantity > 0 )
-            displayMessage(priceMessage);
-        else
-            displayMessage("Please have a cup.");
+        String message = createOrderSummary(price);
+
+        displayMessage(message);
     }
 
     /**
      * This method displays the given quantity value on the screen.
+     * @param numCoffee is the number of coffees ordered
      */
-    private void display(int number)
+    private void displayQuantity(int numCoffee)
     {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numCoffee);
     }
 
     /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number)
-    {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    /**
-     * This method increments the quantity when clicking the - button.
+     * This method increments the quantity when clicking the plus button.
      */
     public void increment(View view)
     {
         quantity++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
-     * This method decrements the quantity when clicking the - button.
+     * This method decrements the quantity when clicking the minus button.
      */
     public void decrement(View view)
     {
         if( quantity > 0 )
             quantity--;
 
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given text on the screen.
+     * @param  message is what the TextView should say.
      */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+    private void displayMessage(String message)
+    {
+        TextView orderSumTextView = (TextView) findViewById(R.id.order_sum_text_view);
+        orderSumTextView.setText(message);
+    }
+
+    /**
+     * Calculates the price of the order based on the current quantity.
+     * @return total price
+     */
+    private int calculatePrice()
+    {
+        return quantity * 5;
+    }
+
+    /**
+     * Builds a string of the order's information
+     * @param price the cost of a cup of coffee
+     * @return order details
+     */
+    private String createOrderSummary(int price)
+    {
+        String summary;
+
+        if( quantity > 0 )
+        {
+            summary = "Name: FirstName LastName\nQuantity: " + quantity
+                    + "\nTotal: $" + price
+                    + "\nThank You!";
+        }
+        else
+            summary = "Please have a cup.";
+
+        return summary;
     }
 }
