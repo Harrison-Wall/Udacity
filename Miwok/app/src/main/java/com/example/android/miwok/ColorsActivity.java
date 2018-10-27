0f7fb28076efa2121d43030fd8199b2530a16408
Harrison Wall <harrisonwall99@yahoo.com>
2018-10-27 15:29:05 -0400

    Added Audio playerback to Colors

--- Miwok/app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,12 +1,17 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity
 {
+    private MediaPlayer musicBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -14,16 +19,16 @@ public class ColorsActivity extends AppCompatActivity
         setContentView(R.layout.word_list);
 
         // Create the list of colors
-        ArrayList<Word> words = new ArrayList<Word>();
+        final ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("Red", "weṭeṭṭi", R.drawable.color_red));
-        words.add(new Word("Green", "chokokki", R.drawable.color_green));
-        words.add(new Word("Brown", "ṭakaakki", R.drawable.color_brown));
-        words.add(new Word("Gray", "ṭopoppi", R.drawable.color_gray));
-        words.add(new Word("Black", "kululli", R.drawable.color_black));
-        words.add(new Word("White", "kelelli", R.drawable.color_white));
-        words.add(new Word("Dusty Yellow", "ṭopiisә", R.drawable.color_dusty_yellow));
-        words.add(new Word("Mustard Yellow", "chiwiiṭә", R.drawable.color_mustard_yellow));
+        words.add(new Word("Red", "weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
+        words.add(new Word("Green", "chokokki", R.drawable.color_green, R.raw.color_green));
+        words.add(new Word("Brown", "ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new Word("Gray", "ṭopoppi", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new Word("Black", "kululli", R.drawable.color_black, R.raw.color_black));
+        words.add(new Word("White", "kelelli", R.drawable.color_white, R.raw.color_white));
+        words.add(new Word("Dusty Yellow", "ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word("Mustard Yellow", "chiwiiṭә", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
 
         // Make a list item view from the words ArrayList
         WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_colors);
@@ -33,5 +38,17 @@ public class ColorsActivity extends AppCompatActivity
 
         // Add the list to the layout
         listView.setAdapter(itemsAdapter);
+
+        // set what to play when clicked
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+            {
+                // Create the media player
+                musicBox = MediaPlayer.create(ColorsActivity.this, words.get(i).getmRawResID() ); //use the position (i) to play the file associated with the word.
+                musicBox.start();
+            }
+        });
     }
 }
