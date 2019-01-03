5e650ac2d22999ead3adf187c8e2f4e6125c60a0
Harrison Wall <harrisonwall99@yahoo.com>
2019-01-03 16:20:37 -0500

    Magnitude now changes color based on severtiy

--- QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,11 +1,14 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.graphics.drawable.GradientDrawable;
 
 import java.util.ArrayList;
 
@@ -29,34 +32,80 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake>
         }
 
         // Get the object located at this position in the list
-        Earthquake currentAndroidFlavor = getItem(position);
+        Earthquake currentEarthquake = getItem(position);
 
         TextView locTextView = (TextView) listItemView.findViewById(R.id.eq_loc);
         TextView distTextView = (TextView) listItemView.findViewById(R.id.eq_dist);
 
-        if( currentAndroidFlavor.getLocation().contains("of") )
+        if( currentEarthquake.getLocation().contains("of") )
         {
-            String locParts[] = currentAndroidFlavor.getLocation().split("of ");
+            String locParts[] = currentEarthquake.getLocation().split("of ");
             distTextView.setText(locParts[0]+"OF");
             locTextView.setText(locParts[1]);
         }
         else
         {
             distTextView.setText("NEAR THE");
-            locTextView.setText(currentAndroidFlavor.getLocation());
+            locTextView.setText(currentEarthquake.getLocation());
         }
 
         TextView magTextView = (TextView) listItemView.findViewById(R.id.eq_mag);
-        magTextView.setText("" + currentAndroidFlavor.getMag() );
+        magTextView.setText("" + currentEarthquake.getMag() );
 
+        // Set the proper background color on the magnitude circle.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magTextView.getBackground();
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMag());
+        magnitudeCircle.setColor(magnitudeColor);
 
         TextView dateView = (TextView) listItemView.findViewById(R.id.eq_date);
-        dateView.setText(currentAndroidFlavor.getDate());
+        dateView.setText(currentEarthquake.getDate());
 
 
         TextView timeView = (TextView) listItemView.findViewById(R.id.eq_time);
-        timeView.setText(currentAndroidFlavor.getTime());
+        timeView.setText(currentEarthquake.getTime());
 
         return listItemView;
+    }
+
+    public int getMagnitudeColor( double pMagnitude )
+    {
+        int retVal;
+
+        switch( (int) Math.floor(pMagnitude) )
+        {
+            case 0:
+            case 1:
+                retVal = R.color.magnitude1;
+                break;
+            case 2:
+                retVal = R.color.magnitude2;
+                break;
+            case 3:
+                retVal = R.color.magnitude3;
+                break;
+            case 4:
+                retVal = R.color.magnitude4;
+                break;
+            case 5:
+                retVal = R.color.magnitude5;
+                break;
+            case 6:
+                retVal = R.color.magnitude6;
+                break;
+            case 7:
+                retVal = R.color.magnitude7;
+                break;
+            case 8:
+                retVal = R.color.magnitude8;
+                break;
+            case 9:
+                retVal = R.color.magnitude9;
+                break;
+            default:
+                retVal = R.color.magnitude10plus;
+                break;
+        }
+
+        return ContextCompat.getColor(getContext(), retVal);
     }
 }
