fb2abcb54e79bdc25c6945e88e4ebd18995a864d
Harrison Wall <harrisonwall99@yahoo.com>
2019-01-12 11:40:45 -0500

    Added AsyncTask to EarthquakeActivity

--- QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,7 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -25,6 +26,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
@@ -71,5 +73,20 @@ public class EarthquakeActivity extends AppCompatActivity {
                 startActivity(websiteIntent);
             }
         });
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask <String, Void, List<Earthquake>>
+    {
+        @Override
+        protected List<Earthquake> doInBackground(String... urls)
+        {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(List<Earthquake> pEquakeList)
+        {
+            return;
+        }
     }
 }
