71bbb54120beaaaf3587039c2bc37fa6e26bd8a6
Harrison Wall <harrisonwall99@yahoo.com>
2019-01-12 12:19:27 -0500

    Add URL and JSON creation funtions to Utils

--- QuakeReport/app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -1,12 +1,23 @@
 package com.example.android.quakereport;
 
+import android.renderscript.ScriptGroup;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+
+import static com.example.android.quakereport.EarthquakeActivity.LOG_TAG;
 
 public final class QueryUtils
 {
@@ -25,8 +36,110 @@ public final class QueryUtils
     }
 
     /**
-     * Set up a URL connection to the USGS_URL
+     * Creates a URL object from the USGS_URL
      */
+    private URL createURL(String pURLString)
+    {
+        URL url = null;
+
+        try
+        {
+            url = new URL(pURLString);
+        }
+        catch(MalformedURLException e)
+        {
+            Log.e(LOG_TAG, "Problem Building URL", e);
+
+        }
+
+        return url;
+    }
+
+    /**
+     * Sets up a URL Connection
+     */
+    private String getJSONReponse(URL url)
+    {
+        String jResponse = "";
+
+
+        if( url == null ) // If no URL return empty
+        {
+            return jResponse;
+        }
+
+        HttpURLConnection urlConnect = null;
+        InputStream inStream = null;
+
+        try
+        {
+            // Set up the connection
+            urlConnect = (HttpURLConnection) url.openConnection();
+            urlConnect.setReadTimeout(10000);
+            urlConnect.setConnectTimeout(15000);
+            urlConnect.setRequestMethod("GET");
+            urlConnect.connect();
+
+            if( urlConnect.getResponseCode() == 200 ) // If successful
+            {
+                // Read Input from URL
+                inStream = urlConnect.getInputStream();
+                jResponse = readFromStream(inStream);
+            }
+            else
+            {
+                Log.e(LOG_TAG, "Error in connecting to URL: " + urlConnect.getResponseCode());
+            }
+        }
+        catch (IOException e)
+        {
+            Log.e(LOG_TAG, "IO Exception", e);
+        }
+        finally
+        {
+            // Clean up connections and Input Stream
+            if(urlConnect != null)
+                urlConnect.disconnect();
+
+            if(inStream != null)
+            {
+                // Any exception would be caught above. This is stupid.
+                try
+                {
+                    inStream.close();
+                }
+                catch (IOException e)
+                {
+                    Log.e(LOG_TAG, "IO Exception", e);
+                }
+            }
+        }
+
+        return jResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a JSON response
+     */
+    private String readFromStream(InputStream inStream) throws IOException
+    {
+        StringBuilder output = new StringBuilder();
+
+        if( inStream != null ) // Check that connection was made
+        {
+            InputStreamReader inputReader = new InputStreamReader(inStream, Charset.forName("UTF-8"));
+            BufferedReader bReader = new BufferedReader(inputReader);
+            String line = bReader.readLine();
+
+            while(line != null) // While there is still info to read
+            {
+                output.append(line);
+                line = bReader.readLine();
+            }
+        }
+
+        return output.toString();
+    }
 
     /**
      * Return a list of {@link Earthquake} objects that has been built up from
