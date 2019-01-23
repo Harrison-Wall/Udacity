4c6c891890940432717d6be3b0279f9bc6f89242
Harrison Wall <harrisonwall99@yahoo.com>
2019-01-23 11:15:55 -0500

    Added Helper Class to create SQL table

---
+++ Pets/app/src/main/java/com/example/android/pets/Data/PetDbHelper.java
@@ -0,0 +1,38 @@
+package com.example.android.pets.Data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.pets.Data.PetContract.PetEntry;
+
+public class PetDbHelper extends SQLiteOpenHelper
+{
+    public static final String LOG_TAG = PetDbHelper.class.getSimpleName();
+    public static final String DATABASE_NAME = "shelter.db";        // Name of the file with database info
+    public static final int DATABASE_VERSION = 1;                               // Increment if schema is changed
+
+    public PetDbHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
+                                + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                                + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL,"
+                                + PetEntry.COLUMN_PET_BREED + " TEXT,"
+                                + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL,"
+                                + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        // Still at version 1, would have to issue an update command if otherwise
+    }
+
+}
