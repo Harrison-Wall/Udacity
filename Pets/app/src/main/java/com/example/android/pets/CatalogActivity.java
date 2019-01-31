4ff4799d79db900955e7280dd5729bb583ebca84
Harrison Wall <harrisonwall99@yahoo.com>
2019-01-31 11:11:16 -0500

    Database Info is not printed on screen

--- Pets/app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ Pets/app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -70,15 +70,27 @@ public class CatalogActivity extends AppCompatActivity
         // get a Cursor that contains all rows from the pets table.
         Cursor cursor = db.query(PetEntry.TABLE_NAME, projection, null, null, null, null, null);
 
+        // Add pets to the TextView
+        TextView displayView = (TextView) findViewById(R.id.text_view_pet);
         try
         {
-            // Display the number of rows in the Cursor
-            TextView displayView = (TextView) findViewById(R.id.text_view_pet);
-            displayView.setText("Number of rows in pets database table: " + cursor.getCount());
+            // Set up header for data
+            displayView.setText("The Pets Table Contains: " + cursor.getCount() + " Pets");
+            displayView.append("\n" + PetEntry._ID + " " + PetEntry.COLUMN_PET_NAME + " " + PetEntry.COLUMN_PET_BREED
+                    + " " + PetEntry.COLUMN_PET_GENDER + " " + PetEntry.COLUMN_PET_WEIGHT);
+
+            // Add all cursor info to the TextView
+            while( cursor.moveToNext() )
+            {
+                displayView.append("\n" + cursor.getString( cursor.getColumnIndex(PetEntry._ID)) + " - "
+                        + cursor.getString( cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME)) + " - "
+                        + cursor.getString( cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED)) + " - "
+                        + cursor.getString( cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER)) + " - "
+                        + cursor.getString( cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT)) );
+            }
         }
-        finally
+        finally // Clean up Cursor
         {
-            // close the cursor
             cursor.close();
         }
     }
@@ -96,6 +108,8 @@ public class CatalogActivity extends AppCompatActivity
         long newRowID = db.insert(PetEntry.TABLE_NAME, null, values);
 
         Log.v("insertPet()", ": New Row # - "+newRowID);
+
+        displayDatabaseInfo();
     }
 
     @Override
