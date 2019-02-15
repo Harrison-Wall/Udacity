0ed30f7e78d243d52865058c4f113146d0acf8db
Harrison Wall <harrisonwall99@yahoo.com>
2019-02-15 11:56:08 -0500

    ContentProvider arguments made clearer

--- Pets/app/src/main/java/com/example/android/pets/Data/PetProvider.java
+++ Pets/app/src/main/java/com/example/android/pets/Data/PetProvider.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 
 public class PetProvider extends ContentProvider
 {
+    // Gives access to pets database
     private PetDbHelper mHelper;
 
     @Override
@@ -16,32 +17,37 @@ public class PetProvider extends ContentProvider
         return true;
     }
 
+    // Perform Query at given URI. Use the given projection, selection, selection arguments, and sort order.
     @Override
-    public Cursor query(Uri uri, String[] args, String s, String[] strings1, String s1)
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder)
     {
         return null;
     }
 
+    // Return the MIME type of the URI content
     @Override
     public String getType(Uri uri)
     {
         return null;
     }
 
+    // Insert new ContentValues into the URI
     @Override
     public Uri insert(Uri uri, ContentValues cvalues)
     {
         return null;
     }
 
+    // Delete data from the URI matching the selection and selectionArgs
     @Override
-    public int delete(Uri uri, String s, String[] strings)
+    public int delete(Uri uri, String selection, String[] selectionArgs)
     {
         return 0;
     }
 
+    // Update the data in the URI at the selection with new ContentValues
     @Override
-    public int update(Uri uri, ContentValues cvalues, String s, String[] strings)
+    public int update(Uri uri, ContentValues cvalues, String selection, String[] selectionArgs)
     {
         return 0;
     }
